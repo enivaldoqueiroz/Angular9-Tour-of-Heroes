fd8e56931717b18eec9819f4cfe47ecd477ba2e4
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2020-10-24 17:40:06 -0300

    Criação da lista de Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  //Propriedade Hero
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
-  ngOnInit(): void {
-    
+  ngOnInit() {
   }
-   
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
 
 
