138bc2519023889167b1f3b3c41bdc54603d4e44
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2020-10-25 10:06:17 -0300

    Criação do componente mesange e serviço mensage, teste conforme imagem 004_IMG

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 
 @Component({
@@ -12,7 +12,7 @@ export class HeroDetailComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
